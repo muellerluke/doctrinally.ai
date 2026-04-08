4da9eded1a80c12bcd016b19fad48577250bc280
muellerluke <66047826+muellerluke@users.noreply.github.com>
2026-04-08 18:21:31 -0500

    added fb pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,6 +89,19 @@ export default function RootLayout({
             __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','AW-18068029031');gtag('config','G-4PJMBW2762');`,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1272174814466979');fbq('track','PageView');`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1272174814466979&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className="min-h-full flex flex-col bg-background">
         <AuthSessionProvider>
